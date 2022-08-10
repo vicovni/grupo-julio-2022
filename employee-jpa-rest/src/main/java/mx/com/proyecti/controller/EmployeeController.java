48f0a836559e2e148076a1ca247bec2b69e0a19c
Victor Calderon <vicovni@gmail.com>
2022-08-09 20:55:32 -0500

    se incluye código referente a la clase de servicios web REST parte 2

--- employee-jpa-rest/src/main/java/mx/com/proyecti/controller/EmployeeController.java
+++ employee-jpa-rest/src/main/java/mx/com/proyecti/controller/EmployeeController.java
@@ -3,7 +3,12 @@ package mx.com.proyecti.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,4 +32,26 @@ public class EmployeeController {
 	List<Employee> getAllEmployees(){
 		return employeeService.getAllEmployees();
 	}
+	
+	@PostMapping("/employees")	//HTTP POST
+	Long insertEmployee(@RequestBody Employee employee) {
+		return employeeService.insertEmployee(employee);
+	}
+	
+	//http://localhost:8090/employees/2
+	@GetMapping("/employees/{id}")	//HTTP GET
+	Employee getEmployee(@PathVariable Long id) { //@PathVariable relaciona el valor de entrada del método con el valor definido en getMapping {id}
+		return employeeService.getEmployee(id);
+	}
+	
+	@PutMapping("/employees") //HTTP PUT
+	Boolean updateEmployee(@RequestBody Employee employee) {
+		return employeeService.updateEmployee(employee);
+	}
+	
+	@DeleteMapping("/employees/{id}") //HTTP DELETE
+	Boolean deleteEmployee(@PathVariable Long id) {
+		return employeeService.deleteEmployee(id);
+	}
+	
 }
